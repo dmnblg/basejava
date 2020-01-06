f18c07d1b560b68f4fc78b90ac35b55ce765c574
Васильев Александр <deadman.alex@gmail.com>
2020-01-06 11:13:14 +0300

    Первый коммит с гитхаба
    Просто пробую отредактировать код на сайте

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -7,11 +7,7 @@ public class ArrayStorage {
     Resume[] storage = new Resume[10000];
 
     void clear() {
-        System.out.println("try to clear");
-        for (int i = 0; i < storage.length; i++) {
-            storage[i] = null;
-        }
-
+        Arrays.fill(storage, null);
     }
 
     void save(Resume r) {
