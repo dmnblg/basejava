a1acb45d3935f247a07b3919ba1d3756f697fa1f
dmnblg <deadman.alex@gmail.com>
2020-01-06 12:49:17 +0300

    Реализация класса ArrayStorage

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -1,3 +1,4 @@
+import java.lang.reflect.Array;
 import java.util.Arrays;
 
 /**
@@ -11,23 +12,57 @@ public class ArrayStorage {
     }
 
     void save(Resume r) {
+        int position = GetFirstNullIndex();
+        Array.set(storage, position, r);
     }
 
     Resume get(String uuid) {
-        return null;
+        Resume result = new Resume();
+
+        int i = GetIndexByUuid(uuid);
+        if (i > -1) {
+            result = storage[i];
+        }
+        return result;
     }
 
     void delete(String uuid) {
+        int del = GetIndexByUuid(uuid);
+        int last = GetFirstNullIndex();
+        storage[del] = storage[last - 1];
+        storage[last - 1] = null;
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return new Resume[0];
+        int lastIndex = GetFirstNullIndex();
+        return Arrays.copyOf(storage, lastIndex);
     }
 
     int size() {
+        return GetFirstNullIndex();
+    }
+
+    int GetFirstNullIndex() {
+        for (int i = 0; i < storage.length; i++) {
+            if (storage[i] == null) {
+                return i;
+            }
+        }
         return 0;
+    }
+
+    int GetIndexByUuid(String uuid) {
+        int r = -1;
+        int lastIndex = GetFirstNullIndex();
+
+        for (int i = 0; i < lastIndex; i++) {
+            if (storage[i].uuid.equals(uuid)) {
+                r = i;
+            }
+        }
+        return r;
     }
 }
