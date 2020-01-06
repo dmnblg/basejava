cf00f9752f050c2ac9add68302ec605281d35aaf
dmnblg <deadman.alex@gmail.com>
2020-01-06 14:37:31 +0300

    Исправил решение в соответствии с замечаниями. (Итерация 1)

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -1,4 +1,3 @@
-import java.lang.reflect.Array;
 import java.util.Arrays;
 
 /**
@@ -6,63 +5,50 @@ import java.util.Arrays;
  */
 public class ArrayStorage {
     Resume[] storage = new Resume[10000];
+    int size;
 
     void clear() {
-        Arrays.fill(storage, null);
+        Arrays.fill(storage,0, size,null);
+        size = 0;
     }
 
     void save(Resume r) {
-        int position = GetFirstNullIndex();
-        Array.set(storage, position, r);
+        storage[size++] = r;
     }
 
     Resume get(String uuid) {
-        Resume result = new Resume();
 
-        int i = GetIndexByUuid(uuid);
+        int i = getIndexByUuid(uuid);
         if (i > -1) {
-            result = storage[i];
+            return storage[i];
         }
-        return result;
+        return null;
     }
 
     void delete(String uuid) {
-        int del = GetIndexByUuid(uuid);
-        int last = GetFirstNullIndex();
-        storage[del] = storage[last - 1];
-        storage[last - 1] = null;
+        int del = getIndexByUuid(uuid);
+        storage[del] = storage[size - 1];
+        storage[size-- - 1] = null;
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        int lastIndex = GetFirstNullIndex();
-        return Arrays.copyOf(storage, lastIndex);
+        return Arrays.copyOf(storage, size);
     }
 
     int size() {
-        return GetFirstNullIndex();
+        return size;
     }
 
-    int GetFirstNullIndex() {
-        for (int i = 0; i < storage.length; i++) {
-            if (storage[i] == null) {
+    int getIndexByUuid(String uuid) {
+
+        for (int i = 0; i < size; i++) {
+            if (storage[i].uuid.equals(uuid)) {
                 return i;
             }
         }
-        return 0;
-    }
-
-    int GetIndexByUuid(String uuid) {
-        int r = -1;
-        int lastIndex = GetFirstNullIndex();
-
-        for (int i = 0; i < lastIndex; i++) {
-            if (storage[i].uuid.equals(uuid)) {
-                r = i;
-            }
-        }
-        return r;
+        return -1;
     }
 }
